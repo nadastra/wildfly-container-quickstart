6ec353e51961938054e1cb084c9daac8c349e8c1
nadastra <40881721+nadastra@users.noreply.github.com>
2022-03-21 02:11:07 +1100

    Create authRedirect.js

---
+++ src/main/webapp/authRedirect.js
@@ -0,0 +1,45 @@
+// Create the main myMSALObj instance
+// configuration parameters are located at authConfig.js
+const myMSALObj = new msal.PublicClientApplication(msalConfig);
+
+let username = "";
+
+/**
+ * A promise handler needs to be registered for handling the
+ * response returned from redirect flow. For more information, visit:
+ * 
+ */
+myMSALObj.handleRedirectPromise()
+    .then(handleResponse)
+    .catch((error) => {
+        console.error(error);
+        throw error;
+    });
+
+/*function selectAccount () {
+    const currentAccounts = myMSALObj.getAllAccounts();
+
+    if (!currentAccounts) {
+        return;
+    } else if (currentAccounts.length > 1) {
+        // Add your account choosing logic here
+        console.warn("Multiple accounts detected.");
+    } else if (currentAccounts.length === 1) {
+        username = currentAccounts[0].username;
+        welcomeUser(username);
+        updateTable();
+    }
+}*/
+
+function handleResponse(response) {
+    if (response !== null) {
+        //username = response.account.username;
+      return response.accessToken;
+    } else {        
+        //selectAccount();
+    }
+}
+
+function login() {
+    myMSALObj.loginRedirect(loginRequest);
+}
